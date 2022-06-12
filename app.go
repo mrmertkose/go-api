9c30f673e34789f7ce2e5209677259cccc93368b
Mert Köse <mertkose@aof.anadolu.edu.tr>
2022-06-12 23:06:48 +0300

    update

--- app.go
+++ app.go
@@ -6,13 +6,13 @@ import (
 	"log"
 )
 
-func main() {
-
+func init() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Print("Error loading .env file")
 	}
+}
 
+func main() {
 	database.Connect()
-
 }
--- models/user.go
+++ models/user.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	Id        uint      `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email" gorm:"unique"`
-	Password  []byte    `json:"-"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	Id        uint
+	Name      string
+	Email     string `gorm:"unique"`
+	Password  []byte
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
